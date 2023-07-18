816b1c27346a95b2ca3397c992a91d9d01c01665
ayd111n <leylaagursoy@hotmail.com>
2023-07-18 13:14:57 +0100

    Add game function for multiple rounds and a score

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+// Variables for answers and scores
 let playerAnswer = ' '
 let computerAnswer = ' '
+let playerScore = 0
+let computerScore = 0
 
+// Function for the players choice and returning the choice to console
 function playerChoice() {
     let question = prompt('Rock, paper or scissors?')
 
@@ -24,6 +28,7 @@ function playerChoice() {
     }
 }
 
+//Function for computer choice using random choice between 1 and 3
 function computerChoice() {
     const random = Math.floor(Math.random() * ((3 - 1) + 1)) + 1;
     
@@ -43,6 +48,9 @@ function computerChoice() {
     }
 }
 
+// Playing a round which compares player and computer answers to each other
+// to declare who wins each rounc
+// Score adds up after each round
 function playRound() {
 
     if (computerAnswer === 'Rock' && playerAnswer === 'Rock' || computerAnswer === 'Paper' && playerAnswer === 'Paper' || computerAnswer === 'Scissors' && playerAnswer === 'Scissors') {
@@ -51,17 +59,53 @@ function playRound() {
 
     else if (computerAnswer === 'Rock' && playerAnswer === 'Scissors' || computerAnswer === 'Paper' && playerAnswer === 'Rock' || computerAnswer === 'Scissors' && playerAnswer === 'Paper') {
         console.log('Computer wins!');
+        computerScore = computerScore + 1
     }
 
     else if (computerAnswer === 'Rock' && playerAnswer === 'Paper' || computerAnswer === 'Paper' && playerAnswer === 'Scissors' || computerAnswer === 'Scissors' && playerAnswer === 'Rock') {
         console.log('Player wins!');
+        playerScore = playerScore + 1
     }
 
     else {
         console.log('Something went wrong.');
     }
+
+    console.log('Computer score is: ' + computerScore + ' and Player score is: ' + playerScore)
 }
 
-playerChoice()
-computerChoice()
-playRound()
+// Totals up player and computer scores for an overall winner
+function endGame() {
+    if (playerScore === computerScore) {
+        console.log('Score tied! Thanks for playing.')
+    }
+
+    else if (playerScore > computerScore) {
+        console.log('Player wins the game! Thanks for playing.')
+    }
+
+    else if (computerScore > playerScore) {
+        console.log('Computer wins the game! Thanks for playing.')
+    }
+}
+
+function game() {
+    playerChoice()
+    computerChoice()
+    playRound()
+    playerChoice()
+    computerChoice()
+    playRound()
+    playerChoice()
+    computerChoice()
+    playRound()
+    playerChoice()
+    computerChoice()
+    playRound()
+    playerChoice()
+    computerChoice()
+    playRound()
+    endGame()
+}
+
+game()
