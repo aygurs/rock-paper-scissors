1377e385a504af0de0bbe8a7375d8b440ad60072
ayd111n <leylaagursoy@hotmail.com>
2023-08-19 13:33:30 +0100

    Update game logic

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 let playerScore = 0
 let computerScore = 0
 let roundNumber = 0
+let gameActive = true
+
+function disableButtons() {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+}
 
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * ((3 - 1) + 1)) + 1;
@@ -24,14 +31,20 @@ const paperButton = document.getElementById('paper')
 const scissorsButton = document.getElementById('scissors')
 
 rockButton.addEventListener('click', function() {
+    roundNumber +=1;
+    roundCheck();
     playRound('rock');
 });
 
 paperButton.addEventListener('click', function() {
+    roundNumber +=1;
+    roundCheck();
     playRound('paper');
 });
 
 scissorsButton.addEventListener('click', function() {
+    roundNumber +=1;
+    roundCheck();
     playRound('scissors');
 });
 
@@ -47,24 +60,21 @@ function playRound(playerSelection) {
     const resultInfo = document.querySelector('.result');
 
     const infoText = document.querySelector('.info');
-    infoText.textContent = 'Player chose: ' + capitalizeFirstLetter(playerSelection) 
+    infoText.textContent = 'You chose: ' + capitalizeFirstLetter(playerSelection) 
     + ' and Computer chose: ' + capitalizeFirstLetter(computerSelection);
 
     if (playerSelection === computerSelection) {
-        roundNumber +=1
         resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You tied!'
     }
 
     else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             computerScore += 1
-            roundNumber +=1
             resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Paper beats rock.'
         }
 
         else if (computerSelection === 'scissors') {
             playerScore += 1
-            roundNumber +=1
             resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Rock beats scissors.'
         }
     }
@@ -72,12 +82,10 @@ function playRound(playerSelection) {
     else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
             playerScore += 1
-            roundNumber +=1
             resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Paper beats rock.'
         }
         else if (computerSelection === 'scissors') {
             computerScore += 1
-            roundNumber +=1
             resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Scissors beats paper.'
         }
     }
@@ -85,12 +93,10 @@ function playRound(playerSelection) {
     else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
             computerScore += 1
-            roundNumber +=1
             resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Rock beats scissors.'
         }
         else if (computerSelection === 'paper') {
             playerScore += 1
-            roundNumber +=1
             resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Scissors beats paper.'
         }
     }
@@ -106,34 +112,36 @@ function roundCheck() {
     }
 
     else if (roundNumber >= 5){
-        endGame()
+        disableButtons();
     }
 }
 
+
 function endGame() {
+
+
+    if (roundNumber < 5) {
+        return;
+    }
+
+    else if (roundNumber >= 5) {
+        
+        const endInfo = document.querySelector('.end');
     
-    if ((playerScore && computerScore > 0) && (playerScore === computerScore)) {
-        return 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
-        + '\nScore tied! Thanks for playing.'
-        // I used this to display a tie as long as both scores are not 0
-        // If both scores are 0, the game has not run correctly
-        // Due to player not entering a valid option
+        if (playerScore === computerScore) {
+            endInfo.textContent = 'End of the game! You tied!'
+        }
+    
+        else if (playerScore > computerScore) {
+            endInfo.textContent = 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
+            + '\nPlayer wins the game! Thanks for playing.'
+        }
+    
+        else if (computerScore > playerScore) {
+            endInfo.textContent = 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
+            + '\nComputer wins the game! Thanks for playing.'
+        }
     }
 
-    else if (playerScore === computerScore) {
-        return 'No score to show.'
-        // If the player choses no valid option in selection then the score will be a tie
-        // But I want to display a different message if it is a tie from playing,
-        // Or if it is a tie from not answering correctly in the first place
-    }
-
-    else if (playerScore > computerScore) {
-        return 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
-        + '\nPlayer wins the game! Thanks for playing.'
-    }
-
-    else if (computerScore > playerScore) {
-        return 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
-        + '\nComputer wins the game! Thanks for playing.'
-    }
+    console.log(playerScore, computerScore);
 }
