618369f21a411248a844d57142742914fcec54cb
ayd111n <leylaagursoy@hotmail.com>
2023-08-19 14:22:27 +0100

    Game logic fix but play again button needs fixing

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// These variables hold data that will be changed later
 let playerScore = 0
 let computerScore = 0
-let roundNumber = 0
+let roundNumber = 1
 let gameActive = true
 
 function disableButtons() {
@@ -10,138 +9,152 @@ function disableButtons() {
     scissorsButton.disabled = true;
 }
 
-function getComputerChoice() {
-    const randomChoice = Math.floor(Math.random() * ((3 - 1) + 1)) + 1;
-    
-    if (randomChoice === 1) {
-        return 'rock'
-    }
-
-    if (randomChoice === 2) {
-        return 'paper'
-    }
-
-    if (randomChoice === 3) {
-        return 'scissors'
-    }
-}
-
-const rockButton = document.getElementById('rock')
-const paperButton = document.getElementById('paper')
-const scissorsButton = document.getElementById('scissors')
-
-rockButton.addEventListener('click', function() {
-    roundNumber +=1;
-    roundCheck();
-    playRound('rock');
-});
-
-paperButton.addEventListener('click', function() {
-    roundNumber +=1;
-    roundCheck();
-    playRound('paper');
-});
-
-scissorsButton.addEventListener('click', function() {
-    roundNumber +=1;
-    roundCheck();
-    playRound('scissors');
-});
-
 function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-
-function playRound(playerSelection) {
-
-    const computerSelection = getComputerChoice();
-
-    const resultInfo = document.querySelector('.result');
-
-    const infoText = document.querySelector('.info');
-    infoText.textContent = 'You chose: ' + capitalizeFirstLetter(playerSelection) 
-    + ' and Computer chose: ' + capitalizeFirstLetter(computerSelection);
-
-    if (playerSelection === computerSelection) {
-        resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You tied!'
+function roundCheck() {
+    if (roundNumber >= 5) {
+        endGame();
     }
-
-    else if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
-            computerScore += 1
-            resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Paper beats rock.'
-        }
-
-        else if (computerSelection === 'scissors') {
-            playerScore += 1
-            resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Rock beats scissors.'
-        }
-    }
-
-    else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            playerScore += 1
-            resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Paper beats rock.'
-        }
-        else if (computerSelection === 'scissors') {
-            computerScore += 1
-            resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Scissors beats paper.'
-        }
-    }
-
-    else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
-            computerScore += 1
-            resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Rock beats scissors.'
-        }
-        else if (computerSelection === 'paper') {
-            playerScore += 1
-            resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Scissors beats paper.'
-        }
-    }
-
-    const score = document.querySelector('.score');
-    score.textContent = 'P: ' + playerScore + ' C: ' + computerScore;
 }
 
-function roundCheck() {
-
-    if (roundNumber < 5){
-        return
+const playAgainButton = document.querySelector('.again');
+playAgainButton.addEventListener('click', function() {
+    if (!gameActive) {
+        const resultInfo = document.querySelector('.result');
+        const infoText = document.querySelector('.info');
+        const endInfo = document.querySelector('.end');
+        infoText.textContent = '';
+        resultInfo.textContent = '';
+        endInfo.textContent = '';
+        initializeGame();
     }
+});
 
-    else if (roundNumber >= 5){
-        disableButtons();
+initializeGame();
+
+function initializeGame() {
+
+    playerScore = 0;
+    computerScore = 0;
+    roundNumber = 1;
+    gameActive = true;
+
+    function getComputerChoice() {
+        const randomChoice = Math.floor(Math.random() * ((3 - 1) + 1)) + 1;
+        
+        if (randomChoice === 1) {
+            return 'rock'
+        }
+    
+        if (randomChoice === 2) {
+            return 'paper'
+        }
+    
+        if (randomChoice === 3) {
+            return 'scissors'
+        }
+    }
+    
+    const rockButton = document.getElementById('rock')
+    const paperButton = document.getElementById('paper')
+    const scissorsButton = document.getElementById('scissors')
+    
+    rockButton.addEventListener('click', function() {
+        if (gameActive) {
+            playRound('rock');
+            roundCheck();
+            roundNumber +=1;
+        }
+    });
+    
+    paperButton.addEventListener('click', function() {
+        if (gameActive) {
+            playRound('paper');
+            roundCheck();
+            roundNumber +=1;
+        }
+    });
+    
+    scissorsButton.addEventListener('click', function() {
+        if (gameActive) {
+            playRound('scissors');
+            roundCheck();
+            roundNumber +=1;
+        }
+    });
+    
+    
+    function playRound(playerSelection) {
+    
+        const computerSelection = getComputerChoice();
+    
+        const resultInfo = document.querySelector('.result');
+    
+        const infoText = document.querySelector('.info');
+        infoText.textContent = 'You chose: ' + capitalizeFirstLetter(playerSelection) 
+        + ' and Computer chose: ' + capitalizeFirstLetter(computerSelection);
+    
+        if (playerSelection === computerSelection) {
+            resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You tied!'
+        }
+    
+        else if (playerSelection === 'rock') {
+            if (computerSelection === 'paper') {
+                computerScore += 1
+                resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Paper beats rock.'
+            }
+    
+            else if (computerSelection === 'scissors') {
+                playerScore += 1
+                resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Rock beats scissors.'
+            }
+        }
+    
+        else if (playerSelection === 'paper') {
+            if (computerSelection === 'rock') {
+                playerScore += 1
+                resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Paper beats rock.'
+            }
+            else if (computerSelection === 'scissors') {
+                computerScore += 1
+                resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Scissors beats paper.'
+            }
+        }
+    
+        else if (playerSelection === 'scissors') {
+            if (computerSelection === 'rock') {
+                computerScore += 1
+                resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You lose! Rock beats scissors.'
+            }
+            else if (computerSelection === 'paper') {
+                playerScore += 1
+                resultInfo.textContent = 'ROUND: ' + roundNumber + '\n You win! Scissors beats paper.'
+            }
+        }
+    
+        const score = document.querySelector('.score');
+        score.textContent = 'P: ' + playerScore + ' C: ' + computerScore;
     }
 }
 
 
 function endGame() {
-
-
-    if (roundNumber < 5) {
-        return;
+    gameActive = false;
+    const endInfo = document.querySelector('.end');
+    
+    if (playerScore === computerScore) {
+        endInfo.textContent = 'End of the game! You tied!'
+     }
+    
+    else if (playerScore > computerScore) {
+        endInfo.textContent = 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
+        + '\nPlayer wins the game! Thanks for playing.'
     }
-
-    else if (roundNumber >= 5) {
-        
-        const endInfo = document.querySelector('.end');
     
-        if (playerScore === computerScore) {
-            endInfo.textContent = 'End of the game! You tied!'
-        }
-    
-        else if (playerScore > computerScore) {
-            endInfo.textContent = 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
-            + '\nPlayer wins the game! Thanks for playing.'
-        }
-    
-        else if (computerScore > playerScore) {
-            endInfo.textContent = 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
-            + '\nComputer wins the game! Thanks for playing.'
-        }
+    else if (computerScore > playerScore) {
+        endInfo.textContent = 'Final score: Computer: ' + computerScore + ' Player: ' + playerScore 
+        + '\nComputer wins the game! Thanks for playing.'
     }
-
-    console.log(playerScore, computerScore);
 }
